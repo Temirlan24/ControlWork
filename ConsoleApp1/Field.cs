56bc304f339cb0ee9064c24dd3cfc27324f20fd8
Temirlan Erzatuly <temirlan.erzatuly@gmail.com>
2019-07-15 21:33:13 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Field.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    class Field
+    {
+        public string[] CultureType { get; set; }
+        public void FillTheField(int numberOfField)
+        {
+             CultureType = new string[numberOfField];
+            for (int i = 0; i < numberOfField; i++)
+            {
+                Console.Write("Выберите кльтуры для {0}: ",i+1);
+                CultureType[i] = Console.ReadLine();
+            }
+        }
+
+        public void CreateTheField(string country,int numberOfField)
+        {
+            FillTheField(numberOfField);
+            Console.Write("Вы создали {0} поле в {1} с культурами: ", numberOfField, country);
+            for (int i = 0; i < numberOfField; i++)
+            {
+                Console.Write("{0}." + CultureType[i] + "\t", i + 1);
+            }
+            SetTheWatering(numberOfField);
+        }
+        public void SetTheWatering(int numberOfField)
+        {
+            for (int i = 0; i < numberOfField; i++)
+            {
+                Console.WriteLine("\nВыберите поле (выберите номер поле):");
+                int.TryParse(Console.ReadLine(), out int fieldNumber);
+                if ((i + 1) == fieldNumber)
+                {
+                    Console.WriteLine("Выберите время начало полива: ");
+                    string userInput = Console.ReadLine();
+                    DateTime.TryParse(userInput, out DateTime result);
+                    //string timeWatering = result.ToString(result,"HH:MM");
+                    Console.WriteLine(result);
+                    Console.WriteLine("Выберите количество раз полива в неделю: ");
+                    int.TryParse(Console.ReadLine(), out int numberInWeek);
+                }
+            }
+        }
+        public void EditField(int numberOfField)
+        {
+            //Console.Write("Выберите количество полей: ");
+            //numberOfField = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfField; i++)
+            {
+                Console.WriteLine(CultureType[i]);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Clear();
+                WriteLine("1.Создавать\n2.Редактировать\n3.Искать\n4.Удалить");
+                WriteLine("Выберите действие: ");
+                int.TryParse(Console.ReadLine(),out int action);
+                Field obj = new Field();
+                int numberOfField;
+                if (action == 1)
+                {
+                    Console.Write("Выберите страну для вашей культуры: ");
+                    string country = Console.ReadLine();
+                    Console.Write("Выберите количество полей: ");
+                    int.TryParse(Console.ReadLine(),out numberOfField);
+                    obj.CreateTheField(country,numberOfField);
+                }
+                if (action == 2)
+                {
+                    obj.EditField(numberOfField);
+                }
+                ReadKey();
+            }
+        }
+    }
+}
